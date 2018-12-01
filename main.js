594993cfe0fede74b5ddd19d11818868ef025772
ycngu <snl123698745@163.com>
2018-12-01 18:18:15 +0800

    ver1.0

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
+$imgs = $('.sliders>img')
+$menu = $('.menu>li')
 $sliders = $('.sliders')
+$menu.first().addClass('active')
 
+$sliders.on('mouseenter'){
+    setTimer()
+}
+
+$sliders.on('mouseeleave'){
+    clearInterval(timer)
+}
+
+$menu.on('click', function (e) {
+    $(e.currentTarget).addClass('active').siblings().removeClass('active')
+    current = $(e.currentTarget).index()
+    gotoPic(current)
+})
+
+
+
+
+function setTimer() {
+    var n = 0
+    return timer = setInterval(function () {
+        if (n > 3) {
+            n = 0
+        }
+        $menu.eq(n).trigger('click')
+        n++
+    }, 2000)
+}
+
+function gotoPic(index) {
+    $imgs.css({
+        'transform': `translate(${-920 * index}px)`
+    })
+}
