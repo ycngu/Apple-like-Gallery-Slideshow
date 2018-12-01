35c2cf7bee18faa85edb231864ffdc88514d6932
ycngu <snl123698745@163.com>
2018-12-01 18:34:30 +0800

    ver 2.0

--- main.js
+++ main.js
@@ -3,31 +3,37 @@ $menu = $('.menu>li')
 $sliders = $('.sliders')
 $menu.first().addClass('active')
 
-$sliders.on('mouseenter'){
-    setTimer()
-}
+timer = setTimer()
 
-$sliders.on('mouseeleave'){
+$sliders.on('mouseleave',()=>{
+    timer = setTimer()
+    console.log('leave')
+})
+
+$sliders.on('mouseenter',()=>{
     clearInterval(timer)
-}
+    console.log('clear')
+})
+
 
 $menu.on('click', function (e) {
     $(e.currentTarget).addClass('active').siblings().removeClass('active')
     current = $(e.currentTarget).index()
     gotoPic(current)
+    n = current
+    console.log(current)
 })
 
 
-
+var n = 0
 
 function setTimer() {
-    var n = 0
-    return timer = setInterval(function () {
+    return setInterval(function () {
         if (n > 3) {
             n = 0
         }
         $menu.eq(n).trigger('click')
-        n++
+        n += 1
     }, 2000)
 }
 
